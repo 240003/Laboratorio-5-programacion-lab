5f607a20c1ed74b1e9aa3b0c1afc13286cc72e1d
favio <favio@LAPTOP-61TGIM22>
2022-09-29 09:31:00 -0600

    Add project files.

---
+++ Laboratorio 5 programacion lab/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5___JAP11295202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero;
+
+            Console.WriteLine("EJERCICIO 1");
+            Console.WriteLine("Ingrese un numero entero");
+            numeroEntero = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroEntero > 0)
+            {
+                Console.WriteLine("Resultado : positivo");
+            }
+            else if (numeroEntero < 0)
+            {
+                Console.WriteLine("Resultado : Negativo");
+            }
+            else if (numeroEntero == 0)
+            {
+                Console.WriteLine("Resultado : el valor ingresado es cero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
